cccf02228a8d6ec305fa15ce9d84f3c2bfde03b3
sherry-id <xivianarthur@gmail.com>
2025-05-16 18:47:23 +0700

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 onload = () => {
     const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
+      document.body.classList.remove("container");
       clearTimeout(c);
     }, 1000);
   };
